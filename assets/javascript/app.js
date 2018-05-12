617812f0655155b79b4ef913a3b743299daf6cf3
Aaron Colbert <aaron-colbert@hotmail.com>
2018-05-12 19:17:12 -0400

    added fav and remove fav

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,11 +5,10 @@ var topics = ["Felix the cat", "Bugs Bunny", "Elmer Fudd", "Popeye", "Huckleberr
 
 var renderButtons = function (array)
 {
-    console.log("renderbuttons");
+    
     $(".button-container").empty();
     for (var i = 0; i < array.length; i++)
     {
-        console.log(array[i])
         var btn = $("<button>");
         btn.attr("class", "btn btn-info button-topic");
         btn.attr("data-name", array[i])
@@ -34,7 +33,7 @@ var GetTopic = function ()
     {
         //Confirm asking add to list if true don't empty if false empty
         $(".img-content").empty();
-
+console.log();
         var results = response.data;
 
         for (var i = 0; i < results.length; i++)
@@ -42,10 +41,14 @@ var GetTopic = function ()
             var still = results[i].images.fixed_height_still.url
             var animated = results[i].images.fixed_height.url
 
-            var gifDiv = $("<div class='item'>");
+            var gifDiv = $("<div class='item card card-default'>");
             var rating = results[i].rating;
             var title = results[i].title;
 
+            //Gif Card Body Div
+            var gifBodyDiv = $("<div class='card-body'>");
+
+            //Add Rating
             var p = $("<p>").text("Rating: " + rating);
             if(title === "")
             {
@@ -53,6 +56,8 @@ var GetTopic = function ()
             }
             var t = $("<p>").text("Title: " + title);
 
+
+            //Creating gif and
             var topicImage = $("<img>");
             topicImage.attr("src", still);
             topicImage.attr("data-still", still);
@@ -60,16 +65,26 @@ var GetTopic = function ()
             topicImage.attr("data-state", "still");
             topicImage.attr("class", "gif");
 
+            //download image
             var downloadImage = $("<img>");
             downloadImage.attr("src", "assets/images/download.png");
             downloadImage.attr("class", "download-img");
 
-            gifDiv.append(t);
-            gifDiv.append(p);
-            gifDiv.append(downloadImage);
+             //Add Favourite image
+             var favImage = $("<img>");
+             favImage.attr("src", "assets/images/add-button.png");
+             favImage.attr("class", "fav-img");
+
+            //Add to Card Body 
+            gifBodyDiv.append(t);
+            gifBodyDiv.append(p);
+            gifBodyDiv.append(downloadImage);
+            gifBodyDiv.append(favImage);
+            //add Card Body to main card
+            gifDiv.append(gifBodyDiv);
+
+            //append New Div to Html section
             gifDiv.prepend(topicImage);
-
-
 
             $(".img-content").prepend(gifDiv);
         }
@@ -80,7 +95,7 @@ var GetTopic = function ()
 $(".img-content").on("click", ".gif", function ()
 {
     var state = $(this).attr("data-state");
-    console.log(state);
+
     if (state === "still")
     {
         $(this).attr("src", $(this).attr("data-animate"));
@@ -109,6 +124,32 @@ $("#add-topic").on("click", function (event)
 });
 
 $(document).on("click", ".button-topic", GetTopic);
+
+$(".img-content").on("click", ".fav-img", function (){
+    console.log("fav click");
+    var card = $(this).closest(".card");
+    console.log(card);
+    var x = card.clone();
+    
+    x.find(".fav-img").attr("src", "assets/images/minus-button.png");
+    x.find(".fav-img").attr("class", "delete-img");
+    x.appendTo($(".fav-content"));
+    
+    
+
+    
+
+
+});
+
+$(".fav-content").on("click", ".delete-img", function (){
+    console.log("remove click");
+    var card = $(this).closest(".card")
+    card.remove();
+    console.log(card);
+
+});
+
 
 
 //Inital Button Render
